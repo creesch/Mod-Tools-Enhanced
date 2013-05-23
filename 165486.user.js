0a33a8b666a43a48f6f0155e3206e5c75eddc769
creesch <e.vosseberg@gmail.com>
2013-05-23 18:05:10 +0200

    version numbering

--- 165486.user.js
+++ 165486.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Mod Tools Enhanced
 // @namespace   http://userscripts.org/scripts/show/165486
-// @version     6.1
+// @version     6.2
 // @include     http://www.reddit.com/*
 // @include     http://reddit.com/*
 // @include     http://*.reddit.com/*
